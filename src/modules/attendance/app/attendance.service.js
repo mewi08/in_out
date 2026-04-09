435454638b966bd495bf86f7d986a3d508879beb
Melanie Elizabeth <melanietello2020@gmail.com>
2026-04-09 09:00:19 -0500

    refactor(attendance-service): replace plain errors with AppError and use UserService for validations

--- src/modules/attendance/app/attendance.service.js
+++ src/modules/attendance/app/attendance.service.js
@@ -1,15 +1,15 @@
 const { AttendanceRepository } = require('../infrastructure/attendance.repository');
-const { UserRepository} = require('../../users/infrastructure/users.repository')
+const { UserService} = require('../../users/app/users.service');
 const { Attendance } = require('../domain/attendance.model');
+const { AppError } = require('../../../shared/utils/appError');
 const { formatWorkedTime } = require('../../../shared/utils/formattedTime');
 
 class AttendanceService {
 
-
     static #validateAttendance(attendance) {
         if (!attendance.isValidType()) {
-            throw { status: 400, message: 'Tipo de asistencia inválido' };
-        }
+            throw new AppError('Tipo de asistencia inválido', 400);
+        };
     }
 
     static #calculateWorkedMinutes(records) {
@@ -19,25 +19,24 @@ class AttendanceService {
         for (const record of records) {
             if (record.isCheckIn()) {
                 lastCheckIn = record.time_stamp;
-            }
+            };
 
             if (record.isCheckOut() && lastCheckIn) {
                 const diff = (record.time_stamp - lastCheckIn) / 60000;
                 total += diff;
                 lastCheckIn = null;
-            }
-        }
+            };
+        };
 
         return total;
-    }
+    };
 
-    // 🔹 registrar asistencia
     static async register(data) {
         const attendance = new Attendance(data);
 
         this.#validateAttendance(attendance);
 
-        const user = await UserRepository.findByCode(attendance.entered_code);
+        const user = await UserService.findByCode(attendance.entered_code);
         
         const last = await AttendanceRepository.findLastByUserId(attendance.user_id);
 
@@ -46,9 +45,9 @@ class AttendanceService {
             const lastRecord = new Attendance(last);
 
             if (lastRecord.isCheckIn() && attendance.isCheckIn()) {
-                throw { status: 400, message: 'Ya existe un check-in sin check-out' };
-            }
-        }
+                throw new AppError('Ya existe un check-in sin check-out', 400);
+            };
+        };
 
         const id = await AttendanceRepository.create(attendance.toJSON());
 
@@ -56,9 +55,8 @@ class AttendanceService {
             id,
             message: attendance.isCheckIn() ? 'Entrada registrada' : 'Salida registrada'
         };
-    }
+    };
 
-    // 🔹 horas trabajadas hoy
     static async getTodayHours(user_id) {
         const today = new Date().toISOString().slice(0, 10);
 
