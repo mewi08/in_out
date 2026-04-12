8f23570d17d4ebda30f454a7822ec77744e4d6f2
Melanie Elizabeth <melanietello2020@gmail.com>
2026-04-12 15:40:34 -0500

    fix(db): fix db setup connection by removing database option

--- scripts/dbSetup.js
+++ scripts/dbSetup.js
@@ -9,7 +9,6 @@ async function setupDatabase() {
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      name: process.env.DB_NAME,
       multipleStatements: true
     });
 
