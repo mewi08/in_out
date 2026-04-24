1071fcb8f35e107d9458d1f368218ad9b63dbf24
Melanie Elizabeth <melanietello2020@gmail.com>
2026-04-24 13:33:00 -0500

    feat: agregar validaciones si exisiste o no el código a los métodos create y update

--- src/modules/users/app/users.service.js
+++ src/modules/users/app/users.service.js
@@ -10,20 +10,24 @@ class UserService {
             throw new AppError('Usuario no encontrado', 404);
         }
         return user;
-    };
+    }
+
     static async #validateDniExists(dni){
         const user = await UserRepository.findByDni(dni);
         if(!user){
             throw new AppError('Usuario no encontrado', 404);
         }
         return user;
-    };
+    }
+
     static async #ensureDniNotExists(dni){
         const user = await UserRepository.findByDni(dni);
         if(user){
             throw new AppError('El DNI ya está registrado', 400);
         }
-    };
+    }
+
+
     static async #validateCodeExists(code){
         const user = await UserRepository.findByCode(code);
         if(!user){
@@ -32,19 +36,26 @@ class UserService {
         return user;
     }
 
+    static async #ensureCodeNotExists(code){
+        const user = await UserRepository.findByCode(code);
+        if(user){
+            throw new AppError('El código ya está registrado', 404);
+        }
+    }    
+
     // ── public methods ─────────────────────────────
 
     static async getAll() {
         return await UserRepository.findAll();
-    };
+    }
 
     static async getById(id) {
         return await this.#validateUserExists(id);
-    };
+    }
 
     static async getByDni(dni) {
         return await this.#validateDniExists(dni);
-    };
+    }
 
     static async create(data) {
         if (!data) {
@@ -52,25 +63,35 @@ class UserService {
         }
         const user = new User(data);
         await this.#ensureDniNotExists(user.dni);
+        await this.#ensureCodeNotExists(user.code);
         const userId = await UserRepository.create(user.toJSON());
         return await UserRepository.findById(userId);
-    };
+    }
 
     static async update(id, data) {
         await this.#validateUserExists(id);
         if (!data) {
             throw new AppError('Datos inválidos para actualizar', 400);
         };
+
         if(data.dni){
             const existing = await UserRepository.findByDni(data.dni);
             if(existing && existing.id !== id){
                 throw new AppError('El DNI ya está registrado', 400);
             }
         };
+
+        if(data.code){
+            const existing = await UserRepository.findByCode(data.code);
+            if(existing && existing.id !== id){
+                throw new AppError('El código ya está registrado', 400);
+            }
+        };
+
         const user = new User(data);
         await UserRepository.update(id, user.toJSON());
         return await UserRepository.findById(id);
-    };
+    }
 
     static async updateStatus(id, is_active) {
         await this.#validateUserExists(id);
@@ -83,7 +104,7 @@ class UserService {
             is_active,
             message: `Usuario ${is_active ? 'activado' : 'desactivado'}`
         };
-    };
+    }
 
     static async verifySecurityCode(security_code) {
         const validCode = process.env.SECURITY_CODE;
