22a7032a967472e95b6f40adb234d03eb40d5209
Melanie Elizabeth <melanietello2020@gmail.com>
2026-04-28 10:46:09 -0500

    feat(users): agregar autenticación y autorización

--- src/modules/users/users.routes.js
+++ src/modules/users/users.routes.js
@@ -5,17 +5,56 @@ const UserController = require('./users.controller');
 const {validateId} = require('../../shared/middleware/param.middleware');
 const {verifyExists} = require('../../shared/middleware/exists.middleware');
 const {validateInfo, validateStatus} = require('../users/domain/users.validator');
+const {authMiddleware, authorizeRoles} = require('../../shared/middleware/auth.middleware');
 
+router.get(
+    '/', 
+    UserController.getAll);
 
-router.get('/', UserController.getAll);
-router.get('/dni/:dni', UserController.getByDni);
-router.get('/code/:code', UserController.getByCode);
-router.post('/verify-security-code', UserController.verifySecurityCode);
+router.get(
+    '/dni/:dni', 
+    UserController.getByDni);
 
-router.get('/:id', validateId(), verifyExists('users'), UserController.getById);
+router.get(
+    '/code/:code', 
+    UserController.getByCode);
+    
+router.post(
+    '/verify-security-code', 
+    UserController.verifySecurityCode);
 
-router.post('/', validateInfo, UserController.create);
-router.put('/:id', validateId(), verifyExists('users'), validateInfo, UserController.update);
-router.patch('/:id/status', validateId(), verifyExists('users'),validateStatus, UserController.updateStatus);
+router.get(
+    '/:id', 
+    validateId(), 
+    verifyExists('users'), 
+    UserController.getById);
+
+router.post(
+    '/', 
+    authMiddleware, 
+    authorizeRoles('admin'), 
+    validateInfo, 
+    UserController.create
+);
+
+router.put(
+    '/:id', 
+    authMiddleware,
+    authorizeRoles('admin'), 
+    validateId(), 
+    verifyExists('users'), 
+    validateInfo, 
+    UserController.update
+);
+
+router.patch(
+    '/:id/status', 
+    authMiddleware,
+    authorizeRoles('admin'), 
+    validateId(), 
+    verifyExists('users'),
+    validateStatus, 
+    UserController.updateStatus
+);
 
 module.exports = router;
