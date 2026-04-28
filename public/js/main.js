01c026f31eaff0de63c3846c507bbab8ad81c68f
Melanie Elizabeth <melanietello2020@gmail.com>
2026-04-28 11:59:48 -0500

    feat(attendance): carga sidebar dinámico y envía tipo en la URL

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,15 @@
+import { loadPartial } from './shared/partials.loader.js';
 import { startClock } from './shared/clock.js';
 
-startClock('time', 'date');
+document.addEventListener('DOMContentLoaded', async () => {
+    await loadPartial('sidebar-container', '/partials/sidebar.html');
+    startClock('time', 'date');
 
-document.getElementById('registerBtn').addEventListener('click', ()=>{
-    window.location.href ='/pages/users/register_profile.html';
-});
+    document.getElementById('checkInBtn').addEventListener('click', ()=>{
+        window.location.href = '/pages/attendance/register_attendance.html?type=check_in';
+    });
 
-document.getElementById('visitBtn').addEventListener('click', ()=>{
-    window.location.href = '/pages/users/visit.html';
-});
-
-document.getElementById('checkOutBtn').addEventListener('click', ()=>{
-    const form = 'checkOutBtn';
-    sessionStorage.setItem('type', form);
-    window.location.href = '/pages/attendance/register_attendance.html'
-});
-
-document.getElementById('checkInBtn').addEventListener('click', ()=>{
-    const form = 'checkInBtn';
-    sessionStorage.setItem('type', form);
-    window.location.href = '/pages/attendance/register_attendance.html';
-});
+    document.getElementById('checkOutBtn').addEventListener('click', ()=>{
+        window.location.href = '/pages/attendance/register_attendance.html?type=check_out'
+    });
+})
