25fa50ea0ce96047a3266651ad5eae8f2e528e20
Melanie Elizabeth <melanietello2020@gmail.com>
2026-05-06 15:32:16 -0500

    feat(dashboard): agrega carga de estadísticas y navegación principal

---
+++ public/js/admin/ui/dashboard.controller.js
@@ -0,0 +1,41 @@
+import { authService } from '../../auth/app/auth.service.js';
+import { showAlert, autoHideAlert, setLoading, clearAlert } from '../../shared/ui/message.ui.js';
+import { loadPartial, renderFooter, setActiveSidebar } from '../../shared/ui/partials.loader.js';
+import { applyRolePermissions } from '../../shared/ui/admin.js';
+import { updateStats} from '../../users/ui/user.table.js';
+import { userService } from '../../users/app/users.service.js';
+let stats = [];
+// ==== ELEMENTS DOM ====
+const user = document.getElementById('user');
+const wh = document.getElementById('workingHours');
+
+// ==== INIT ====
+document.addEventListener('DOMContentLoaded', async()=>{
+    clearAlert('alert');
+
+    await loadPartial('sidebar-container','/partials/sidebar.html');
+    setActiveSidebar('dashboard');
+    applyRolePermissions();
+    renderFooter();
+    loadStats();
+})
+
+// ==== EVENT LISTENER ====
+user.addEventListener('click',()=>{
+    window.location.href = '/pages/users/list_users.html';
+});
+
+wh.addEventListener('click',()=>{
+    window.location.href = '/pages/users/working_hours';
+})
+
+// ==== FUNCTIONS ====
+async function loadStats() {
+    try {
+        stats = await userService.getStats();
+        console.log(stats);
+        updateStats(stats);
+    } catch (error) {
+        console.error('Error cargando stats', error.message);
+    }
+}
