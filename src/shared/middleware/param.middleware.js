61b9e482b598f54ed174682d954ed580ae84858e
Melanie Elizabeth <melanietello2020@gmail.com>
2026-05-06 13:25:47 -0500

    refactor(auth): utiliza clase Response para manejo de respuestas

--- src/shared/middleware/param.middleware.js
+++ src/shared/middleware/param.middleware.js
@@ -1,11 +1,10 @@
+const  Response  = require('../infrastructure/response');
 function validateId(paramName = 'id') {
     return (req, res, next) => {
         const value = req.params[paramName];
 
         if (!value || !/^\d+$/.test(value)) {
-            return res.status(400).json({
-                error: `El parámetro '${paramName}' debe ser un número entero válido`
-            });
+            return Response.sendBadRequest(res, `El parámetro '${paramName}' debe ser un número entero válido`);
         }
 
         req.params[paramName] = Number(value);
