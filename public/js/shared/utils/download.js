3597e1212b8be70cd2dd9d288308ecb56b66e75e
Melanie Elizabeth <melanietello2020@gmail.com>
2026-05-07 08:58:41 -0500

    feat(utils): implementa helper para descargar archivos

---
+++ public/js/shared/utils/download.js
@@ -0,0 +1,8 @@
+export function downloadFile(url, filename) {
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    a.click();
+    window.URL.revokeObjectURL(url);
+}   
