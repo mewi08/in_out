ebc6b46bd269db24f6eb4d1cc024a43235b22d0e
Melanie Elizabeth <melanietello2020@gmail.com>
2026-05-10 21:23:15 -0500

    feat(users): agrega botón para descargar asistencias

--- public/js/users/ui/user.table.js
+++ public/js/users/ui/user.table.js
@@ -14,7 +14,7 @@ export function updateStats(stats) {
     if(statInactive){statInactive.textContent = stats[0].inactive}    
 }
 
-export function renderTable(users, { onEdit, onToggle } = {}) {
+export function renderTable(users, { onEdit, onToggle, onExport } = {}) {
     const tbody = document.getElementById("usersTableBody");
 
     tbody.innerHTML = users.map(u => `
@@ -46,6 +46,12 @@ export function renderTable(users, { onEdit, onToggle } = {}) {
                         data-active="${u.is_active ? 'true' : 'false'}">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#9e97a0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-ban-icon lucide-ban"><circle cx="12" cy="12" r="10"/><path d="M4.929 4.929 19.07 19.071"/></svg>
                     </button>
+
+                    <button class="btn-icon"
+                        data-action="export"
+                        data-id="${u.id}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-download-icon lucide-download"><path d="M12 15V3"/><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><path d="m7 10 5 5 5-5"/></svg>
+                    </button>
                 </div>
             </td>
         </tr>
@@ -64,6 +70,10 @@ export function renderTable(users, { onEdit, onToggle } = {}) {
 
         if (action === 'toggle' && onToggle) {
             onToggle(id, btn.dataset.active === 'true');
+        }
+
+        if (action === 'export' && onExport) {
+            onExport(id);
         }
     };
 }
