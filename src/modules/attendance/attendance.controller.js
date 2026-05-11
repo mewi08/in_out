93eb63f5130d524bd53e5e46d45f439c8ef2190c
Melanie Elizabeth <melanietello2020@gmail.com>
2026-05-10 21:17:40 -0500

    feat(attendance): agrega modos user y batch para generación de archivos Excel y paginación

--- src/modules/attendance/attendance.controller.js
+++ src/modules/attendance/attendance.controller.js
@@ -2,12 +2,13 @@ const { AttendanceService } = require('./app/attendance.service');
 const { Response } = require('../../shared/core/http/response');
 const { generateExcel } = require('../../shared/utils/excel');
 const logger = require('../../shared/infrastructure/logger');
+const { parse } = require('dotenv');
 
 async function create(req, res) {
     try {
         const { code, type } = req.body;
-        const record = await AttendanceService.register({ code, type});
-        logger.info(`Movimiento registrado para usuario ${req.body.code}: ${record.message}`);
+        const record = await AttendanceService.register({ code, type });
+        logger.info(`Movimiento registrado para usuario ${code}: ${record.message}`);
         return Response.sendCreated(res, record);
     } catch (error) {
         logger.error('Error en create attendance', error);
@@ -20,17 +21,17 @@ async function getTodayHours(req, res) {
         const hours = await AttendanceService.getTodayHours(req.params.user_id);
         return Response.sendSuccess(res, hours);
     } catch (error) {
-        logger.error(`Error en getTodayHours attendance (${req.params.user_id})`, error);
+        logger.error(`Error en getTodayHours (${req.params.user_id})`, error);
         return Response.sendError(res, error);
     }
 }
 
 async function getTodayStatus(req, res) {
-    try{
+    try {
         const { user_id } = req.params;
         const status = await AttendanceService.getTodayStatus(user_id);
         return Response.sendSuccess(res, status);
-    }catch(error){
+    } catch (error) {
         logger.error(`Error en getTodayStatus (${req.params.user_id})`, error);
         return Response.sendError(res, error);
     }
@@ -38,7 +39,8 @@ async function getTodayStatus(req, res) {
 
 async function getAttendanceReport(req, res) {
     try {
-        const report = await AttendanceService.getAttendanceReport();
+        const page = parseInt(req.query.page) || 1;
+        const report = await AttendanceService.getAttendanceReport(page);
         return Response.sendSuccess(res, report);
     } catch (error) {
         logger.error('Error en getAttendanceReport', error);
@@ -48,16 +50,19 @@ async function getAttendanceReport(req, res) {
 
 async function exportByUser(req, res) {
     try {
-        const { dni } = req.params;
+        const { user_id } = req.params;
         const { startDate, endDate } = req.query;
-
-        const rows = await AttendanceService.exportByUser(
-            dni,
+        const { rows, user } = await AttendanceService.exportByUser(
+            user_id,
             startDate,
             endDate
         );
-
-        await generateExcel(rows, res, `asistencia_${dni}.xlsx`);
+        await generateExcel(rows, res, {
+            mode: 'user',
+            user,
+            filename:
+                `asistencia_${user.dni}_${startDate}_${endDate}.xlsx`
+        });
     } catch (error) {
         logger.error('Error en exportByUser', error);
         return Response.sendError(res, error);
@@ -67,17 +72,25 @@ async function exportByUser(req, res) {
 async function exportAll(req, res) {
     try {
         const { startDate, endDate } = req.query;
-
         const rows = await AttendanceService.exportAll(
             startDate,
             endDate
         );
-
-        await generateExcel(rows, res, `asistencia_general.xlsx`);
+        await generateExcel(rows, res, {
+            mode: 'batch',
+            filename: `asistencia_general_${startDate}_${endDate}.xlsx`
+        });
     } catch (error) {
         logger.error('Error en exportAll', error);
         return Response.sendError(res, error);
     }
 }
 
-module.exports = { create, getTodayHours, getTodayStatus, getAttendanceReport, exportByUser, exportAll };
+module.exports = {
+    create,
+    getTodayHours,
+    getTodayStatus,
+    getAttendanceReport,
+    exportByUser,
+    exportAll
+};
