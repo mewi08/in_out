426d63d2bff101058572fbef964681a4167d058f
Melanie Elizabeth <melanietello2020@gmail.com>
2026-05-11 15:24:53 -0500

    refactor(attendance): eliminar endpoint sin uso

--- src/modules/attendance/attendance.routes.js
+++ src/modules/attendance/attendance.routes.js
@@ -14,13 +14,6 @@ router.post(
 );
 
 router.get(
-    '/today-hours/:user_id', 
-    validateId('user_id'), 
-    verifyExists('users', 'user_id'), 
-    AttendanceController.getTodayHours
-);
-
-router.get(
     '/today-status/:user_id', 
     validateId('user_id'), 
     verifyExists('users','user_id'), 
