08b16866bcd7ce0ae46289dfd8cadbf60d0bd599
Melanie Elizabeth <melanietello2020@gmail.com>
2026-05-13 12:50:53 -0500

    refactor(users): centraliza manejo global de errores

--- src/modules/users/users.controller.js
+++ src/modules/users/users.controller.js
@@ -1,8 +1,9 @@
 const { UserService } = require('./app/users.service');
 const { Response } = require('../../shared/core/http/response');
+const { AppError } = require('../../shared/core/error/appError');
 const {logger, logError} = require('../../shared/infrastructure/logger');
 const { ActivityLogService } = require('../activity_log/app/activity_log.service');
-async function getAdminUsers(req, res) {
+async function getAdminUsers(req, res, next) {
     try {
         const filters = {
             is_active: req.query.status !== undefined
@@ -18,12 +19,11 @@ async function getAdminUsers(req, res) {
         const users = await UserService.getUsers(filters);
         return Response.sendSuccess(res, users);
     } catch (error) {
-        logError('Error en getAdminUsers', error);
-        return Response.sendError(res, error);
+        next(error);
     }
 }
 
-async function getPublicUsers(req, res) {
+async function getPublicUsers(req, res, next) {
     try{
         const filters = {
             is_active: 1, 
@@ -44,32 +44,29 @@ async function getPublicUsers(req, res) {
         }));
         return Response.sendSuccess(res, safeUsers);
     } catch (error) {
-        logError('Error en getPublicUsers', error);
-        return Response.sendError(res, error);
+        next(error);
     };
 }
 
-async function getStats(req, res) {
+async function getStats(req, res, next) {
     try {
         const stats = await UserService.getStats();
         return Response.sendSuccess(res, stats);
     } catch (error) {
-        logError('Error en getStats', error);
-        return Response.sendError(res, error);
+        next(error);
     };
 }
 
-async function getById(req, res) {
+async function getById(req, res, next) {
     try {
         const user = await UserService.getById(req.params.id);
         return Response.sendSuccess(res, user);
     } catch (error) {
-        logError(`Error en getById user (${req.params.id})`, error);
-        return Response.sendError(res, error);
+        next(error);
     };
 }
 
-async function getByCode(req, res) {
+async function getByCode(req, res, next) {
     try{
         const user = await UserService.getByCodeActive(req.params.code);
         return Response.sendSuccess(res, user);
@@ -79,7 +76,7 @@ async function getByCode(req, res) {
     };
 }
 
-async function create(req, res) {
+async function create(req, res, next) {
     try {
         const user = await UserService.create(req.body);
         logger.info(`Usuario creado: ${user.dni}`);
@@ -90,12 +87,11 @@ async function create(req, res) {
         });
         return Response.sendCreated(res, user);
     } catch (error) {
-        logError('Error en create user', error);
-        return Response.sendError(res, error);
+        next(error);
     }
 }
 
-async function update(req, res) {
+async function update(req, res, next) {
     try {
         const user = await UserService.update(
             req.params.id,
@@ -109,12 +105,11 @@ async function update(req, res) {
         });
         return Response.sendSuccess(res, user);
     } catch (error) {
-        logError(`Error en update user (${req.params.id})`, error);
-        return Response.sendError(res, error);
+        next(error);
     }
 }
 
-async function updateStatus(req, res) {
+async function updateStatus(req, res, next) {
     try {
         const { is_active } = req.body;
         const user =
@@ -137,8 +132,7 @@ async function updateStatus(req, res) {
         );
         return Response.sendSuccess(res, user);
     } catch (error) {
-        logError(`Error en updateStatus user (${req.params.id})`, error);
-        return Response.sendError(res, error);
+        next(error);
     }
 }
 
