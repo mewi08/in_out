a1698d9e9346a2e87e27170fda552fec5b8db869
Melanie Elizabeth <melanietello2020@gmail.com>
2026-05-14 10:04:59 -0500

    refactor(error): centraliza manejo global de errores

--- src/modules/activity_log/activity_log.controller.js
+++ src/modules/activity_log/activity_log.controller.js
@@ -2,16 +2,14 @@ const { ActivityLogService } = require('./app/activity_log.service');
 const { Response } = require('../../shared/core/http/response');
 const logger = require('../../shared/infrastructure/logger');
 
-async function getRecent(req, res) {
+async function getRecent(req, res, next) {
     try {
         const logs = await ActivityLogService.getRecent();
 
         return Response.sendSuccess(res, logs);
 
     } catch (error) {
-        logger.error('Error en getRecent activity logs', error);
-
-        return Response.sendError(res, error);
+        next(error);
     }
 }
 
