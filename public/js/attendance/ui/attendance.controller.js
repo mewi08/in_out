3b0a03a10a94c84fd22cf6396b4f1811a7e6578a
Melanie Elizabeth <melanietello2020@gmail.com>
2026-05-18 13:12:16 -0500

    refactor(attendance): reorganiza estructura del código

--- public/js/attendance/ui/attendance.controller.js
+++ public/js/attendance/ui/attendance.controller.js
@@ -7,133 +7,83 @@ import {
     setLoading
 } from '../../shared/ui/message.ui.js';
 
-// ===== ELEMENTOS DOM =====
+let currentEmployee = null;
+let currentCode = null;
+let currentType = 'check_in';
+
 const step1 = document.getElementById('step-1');
 const step2 = document.getElementById('step-2');
 const stepDots = document.querySelectorAll('.step-dot');
 
+const subtitle = document.getElementById('subtitle');
 const codeInput = document.getElementById('codeInput');
 const validateCodeBtn = document.getElementById('validateCodeBtn');
 const backBtn = document.getElementById('backBtn');
+
 const employeeName = document.getElementById('employeeName');
 const employeeCategory = document.getElementById('employeeCategory');
 const employeeDni = document.getElementById('employeeDni');
 const employeeWorkArea = document.getElementById('employeeWorkArea');
 const avatarInitials = document.getElementById('avatarInitials');
+
 const confirmBtn = document.getElementById('confirmBtn');
 const changeCodeBtn = document.getElementById('changeCodeBtn');
 
-const subtitle = document.getElementById('subtitle');
-
-// ===== VARIABLES =====
-let currentEmployee = null;
-let code = null;
-let currentType = null;
-
-// ===== INITIALIZE =====
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', async () => {
     clearAlert('alert');
-    const type = getTypeFromURL();
-
-    if (type === 'check_in') {
-        subtitle.textContent = 'Registra entrada';
-        currentType = 'check_in';
-    } else if (type === 'check_out') {
-        subtitle.textContent = 'Registra salida';
-        currentType = 'check_out';
-    } else {
-        subtitle.textContent = 'Registrar asistencia';
-        currentType = 'check_in';
-    }
+    setupAttendanceType();
+    setupEvents();
 });
 
-// ===== EVENT LISTENERS =====
-validateCodeBtn.addEventListener('click', ()=>{
-    const codeClean = codeInput.value.trim();
-    if (!codeClean) {
+function setupEvents() {
+    validateCodeBtn.addEventListener('click', handleValidateCode);
+    codeInput.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') { handleValidateCode() };
+    });
+    confirmBtn.addEventListener('click', handleMarkAttendance);
+    changeCodeBtn.addEventListener('click', handleChangeCode);
+    backBtn.addEventListener('click', () => {
+        window.location.href = '/index.html';
+    });
+}
+
+function setupAttendanceType() {
+    const type = getTypeFromURL();
+    if (type === 'check_out') {
+        subtitle.textContent = 'Registra salida';
+        currentType = 'check_out';
+        return;
+    }
+    subtitle.textContent = 'Registra entrada';
+    currentType = 'check_in';
+}
+
+async function handleValidateCode() {
+    const code = codeInput.value.trim();
+    if (!code) {
         showAlert('Ingresa tu código', 'warning', 'alert');
         autoHideAlert('alert');
         return;
     }
-    validateCode(codeClean);
-});
-
-codeInput.addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') validateCodeBtn.click();
-});
-
-backBtn.addEventListener('click', () => {
-    window.location.href = '/index.html';
-});
-
-confirmBtn.addEventListener('click', async () => {
-    markAttendance(currentType);
-});
-
-changeCodeBtn.addEventListener('click', () => {
-    goToStep(1);
-    codeInput.value = '';
-    codeInput.focus();
-    currentEmployee = null;
-    code = null;
-});
-
-// ===== FUNCIONES =====
-async function validateCode(codeClean) {
-    clearAlert('alert');
-    setLoading(validateCodeBtn, true);
-
-    try {
-        const user = await userService.getUserByCode(codeClean);
-        currentEmployee = user;
-        code = codeClean;
-
-        showEmployeeData(currentEmployee);
-        goToStep(2);
-
-    } catch (err) {
-        showAlert(err.message , 'danger', 'alert');
-        autoHideAlert('alert');
-    } finally {
-        setLoading(validateCodeBtn, false);
-    }
+    await validateCode(code);
 }
 
-function showEmployeeData(employee) {
-    employeeName.textContent = `${employee.name} ${employee.last_name}`;
-    employeeCategory.textContent = employee.category;
-    employeeDni.textContent = employee.dni;
-    employeeWorkArea.textContent = employee.work_area || 'No especificado';
-    
-    const initials = `${employee.name[0]}${employee.last_name[0]}`.toUpperCase();
-    avatarInitials.textContent = initials;
-}
-
-function getTypeFromURL(){
-    const params = new URLSearchParams(window.location.search);
-    return params.get('type');
-}
-
-async function markAttendance(type) {
-    if (!currentEmployee || !code) {
+async function handleMarkAttendance() {
+    if (!currentEmployee || !currentCode) {
         showAlert('Usuario no válido', 'danger', 'alert');
         return;
-    };
-    
-    setLoading(confirmBtn, true);
+    }
 
     try {
+        setLoading(confirmBtn, true);
         const attendance = await attendanceService.createRecord({
-            code: code,
-            type: type
+            code: currentCode,
+            type: currentType
         });
-
         showAlert(attendance.message, 'success', 'alert');
-
         setTimeout(() => {
             resetAndGoHome();
         }, 2000);
-
     } catch (err) {
         const isNetwork = err instanceof TypeError;
         showAlert(
@@ -149,20 +99,67 @@ async function markAttendance(type) {
     }
 }
 
-function resetAndGoHome() {
+function handleChangeCode() {
     currentEmployee = null;
-    code = null;
-    codeInput.value = '';  
-    window.location.href = '/index.html';
+    currentCode = null;
+    codeInput.value = '';
+    codeInput.focus();
+    goToStep(1);
+}
+
+async function validateCode(code) {
+    try {
+        clearAlert('alert');
+        setLoading(validateCodeBtn, true);
+        const user = await userService.getUserByCode(code);
+        currentEmployee = user;
+        currentCode = code;
+        renderEmployee(user);
+        goToStep(2);
+    } catch (err) {
+        showAlert(err.message, 'danger', 'alert');
+        autoHideAlert('alert');
+    } finally {
+        setLoading(validateCodeBtn, false);
+    }
+}
+
+function renderEmployee(employee) {
+    employeeName.textContent = `${employee.name} ${employee.last_name}`;
+    employeeCategory.textContent = employee.category;
+    employeeDni.textContent = employee.dni;
+    employeeWorkArea.textContent = employee.work_area || 'No especificado';
+    avatarInitials.textContent = `${employee.name[0]}${employee.last_name[0]}`.toUpperCase();
 }
 
 function goToStep(stepNumber) {
     step1.classList.add('d-none');
-    step2.classList.add('d-none'); 
+    step2.classList.add('d-none');
     document.getElementById(`step-${stepNumber}`).classList.remove('d-none');
+
     stepDots.forEach((dot, index) => {
-        dot.classList.remove('bg-primary', 'bg-secondary');
-        dot.classList.add(index < stepNumber ? 'bg-primary' : 'bg-secondary');
+        dot.classList.remove(
+            'bg-primary',
+            'bg-secondary'
+        );
+
+        dot.classList.add(
+            index < stepNumber
+                ? 'bg-primary'
+                : 'bg-secondary'
+        );
     });
     clearAlert('alert');
+}
+
+function getTypeFromURL() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get('type');
+}
+
+function resetAndGoHome() {
+    currentEmployee = null;
+    currentCode = null;
+    codeInput.value = '';
+    window.location.href = '/index.html';
 }
