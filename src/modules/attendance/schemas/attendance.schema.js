9d7e51ee98a94eb64581d2422eda23ece73ec76f
Melanie Elizabeth <melanietello2020@gmail.com>
2026-05-18 13:40:23 -0500

    feat(attendance): agrega schema de validación para exportación

--- src/modules/attendance/schemas/attendance.schema.js
+++ src/modules/attendance/schemas/attendance.schema.js
@@ -16,4 +16,34 @@ const attendanceSchema = z.object({
         .min(1, 'Tipo de asistencia requerido')
 });
 
-module.exports = { attendanceSchema } ;
+const dateString = z
+    .string({
+        required_error: 'La fecha es obligatoria'
+    })
+    .min(1, 'La fecha no puede estar vacía')
+    .refine((val) => !isNaN(Date.parse(val)), {
+        message: 'Formato de fecha inválido'
+    });
+
+const exportAllSchema = z.object({
+    startDate: dateString,
+    endDate: dateString
+}).refine((data) => {
+    return new Date(data.startDate) <= new Date(data.endDate);
+}, {
+    message: 'La fecha inicio no puede ser mayor a la fecha fin',
+    path: ['startDate']
+});
+
+
+const exportUserSchema = z.object({
+    startDate: dateString,
+    endDate: dateString
+}).refine((data) => {
+    return new Date(data.startDate) <= new Date(data.endDate);
+}, {
+    message: 'La fecha inicio no puede ser mayor a la fecha fin',
+    path: ['startDate']
+});
+
+module.exports = { attendanceSchema, exportAllSchema, exportUserSchema } ;
