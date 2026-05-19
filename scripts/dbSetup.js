90fca55617ed7017510ae5d0fa477813feda69e4
Melanie Elizabeth <melanietello2020@gmail.com>
2026-05-19 11:50:12 -0500

    chore(db): usa variable de entorno para nombre de base de datos

--- scripts/dbSetup.js
+++ scripts/dbSetup.js
@@ -9,6 +9,7 @@ async function setupDatabase() {
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       multipleStatements: true
     });
 
