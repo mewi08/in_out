a1d7eebc33247ff35615bd7843fbc62a3da270c7
Melanie Elizabeth <melanietello2020@gmail.com>
2026-05-20 08:38:27 -0500

    fix(admin): evita acceso sin autenticación

--- public/js/admin/ui/dashboard.controller.js
+++ public/js/admin/ui/dashboard.controller.js
@@ -6,28 +6,26 @@ import { userService } from '../../users/app/users.service.js';
 import { loadActivities } from '../../activity_log/ui/activity_log.render.js';
 let stats = [];
 
-// ==== INIT ====
 document.addEventListener('DOMContentLoaded', async()=>{
     clearAlert('alert');
-
+    if (!requireAuth()) return;
     await loadPartial('sidebar-container','/partials/sidebar.html');
     setActiveSidebar('dashboard');
-    requireAuth();
     applyRolePermissions();
     renderFooter();
     loadStats();
     await loadActivities();
 
     // ==== EVENT LISTENER ====
-    document.getElementById('user').addEventListener('click',()=>{
-        window.location.href = '/pages/users/list_users.html';
+    document.getElementById('btnUser').addEventListener('click',()=>{
+        window.location.href = '/pages/users/users.html';
     });
 
-    document.getElementById('attendance').addEventListener('click',()=>{
+    document.getElementById('btnAttendance').addEventListener('click',()=>{
         window.location.href = '/pages/attendance/list_attendance.html';
     });
 
-    document.getElementById('areas').addEventListener('click', ()=>{
+    document.getElementById('btnAreas').addEventListener('click', ()=>{
         window.location.href = '/pages/work_area/list_workArea.html';
     });
 
