2400d8031b353dd20ca17daa1aa81bf8343d69b2
Melanie Elizabeth <melanietello2020@gmail.com>
2026-05-25 12:38:12 -0500

    refactor(app): simplifica configuración de express

--- src/app.js
+++ src/app.js
@@ -12,7 +12,6 @@ app.use(express.urlencoded({extended : true}));
 app.use(helmet());
 
 app.use(express.static('public'));
-app.use(express.static('views'));
 
 app.use('/api/user', require('./modules/users/users.routes'));
 app.use('/api/attendance', require('./modules/attendance/attendance.routes'));
@@ -20,17 +19,12 @@ app.use('/api/auth', require('./modules/auth/auth.routes'));
 app.use('/api/activity-logs', require('./modules/activity_log/activity_log.routes'));
 app.use('/api/work-area', require('./modules/work_area/work_area.routes'));
 
-app.get('/', (req, res)=>{
-    res.redirect('/views/index.html');
-})
-
 app.use((req, res, next) => {
     const error = new Error('Ruta no encontrada');
     error.statusCode = 404;
     next(error);
 });
 
-// Error global
 app.use((err, req, res, next) => {
     logError('Unhandled error:', err);
     return Response.sendError(res, err);
